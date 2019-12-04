bfe72598afb286de406952f3cb2e463b25dd847e
s3ns3i <eragonsm@gmail.com>
2019-12-05 00:09:44 +0100

    Auth logic for first-use scenario

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,21 @@
 // If it's a private page and there's no payload, redirect.
-export default function (context) {
+export default async function (context) {
   const { store, redirect, route } = context
   const { auth } = store.state
 
 
-  if (!auth.publicPages.includes(route.name) && !auth.payload) {
-    const users = store.dispatch("users/find");
-    if (!users.length) {
-      return redirect('/first-user')
+  // If any user tries to access this page 
+  if (route.name === 'first-user') {
+    const usersNo = await store.dispatch("first-use/find");
+    if (!usersNo.total) {
+      return
     } else {
       return redirect('/login')
     }
+  }
+  // If user is not logged in and tries to access the main page
+  // If user is not logged in and tries to access restricted resource
+  if ((!auth.publicPages.includes(route.name) || route.name === '/') && !auth.payload) {
+    return redirect('/login')
   }
 }
