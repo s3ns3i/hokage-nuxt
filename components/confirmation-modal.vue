3eb97e2e47b248e77990291f7f5ed0e826d9beda
s3ns3i <eragonsm@gmail.com>
2020-04-12 17:00:28 +0200

    User can pass task to next teammate

---
+++ components/confirmation-modal.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">{{ header }}</v-card-title>
+    <v-card-text>
+      {{ message }}
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text @click="$emit('close')">Nie</v-btn>
+      <v-btn color="primary" @click="$emit('confirm')">
+        Tak
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "ConfirmationModal",
+  props: {
+    header: {
+      type: String,
+      required: false,
+      default: "Potwierdzenie"
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- components/translations-menu.vue
+++ components/translations-menu.vue
@@ -37,14 +37,32 @@
           color="success"
           class="ma-3"
           :disabled="isTaskInProgress || isTaskNotTaken"
-          @click="dialogPass = true"
+          @click="onDialogPassOpen"
           >Przekaż dalej</v-btn
         >
       </template>
       <confirmation-modal
-        message="Czy na pewno chcesz zakończyć i przekazać zadanie?"
+        :header="dialogHeader"
+        :message="dialogMessage"
         @close="dialogPass = false"
         @confirm="onPassOnTask"
+      />
+    </v-dialog>
+    <v-dialog v-model="dialogReject" persistent max-width="600">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          color="error"
+          class="ma-3"
+          :disabled="isTaskInProgress || isTaskNotTaken"
+          @click="dialogReject = true"
+          >Odrzuć</v-btn
+        >
+      </template>
+      <confirmation-modal
+        message="Czy na pewno chcesz odrzucić zadanie?"
+        @close="dialogReject = false"
+        @confirm="onRejectTask"
       />
     </v-dialog>
   </v-row>
@@ -62,7 +80,10 @@ export default {
   data() {
     return {
       dialogAbandon: false,
-      dialogPass: false
+      dialogPass: false,
+      dialogReject: false,
+      dialogHeader: "",
+      dialogMessage: ""
     };
   },
   computed: {
@@ -81,10 +102,6 @@ export default {
   },
   methods: {
     onTakeTask() {
-      // send patch to back-end and set userId to user's id
-      // think about transitions
-      // - if there is no transition for this or it's the first group, it's for the first group
-      // - if there is any other group, it's that group's
       const { user } = this.$store.state.auth;
       this.$store.dispatch("task/patch", [
         this.$route.params.id,
@@ -118,7 +135,38 @@ export default {
         .catch(error => console.error(error));
       // send patch to back-end and set userId to null
     },
-    onPassOnTask() {}
+    onDialogPassOpen() {
+      const nextRole = this.getNextRole();
+      if (nextRole) {
+        this.dialogHeader = `Następny: ${nextRole.name}`;
+        this.dialogMessage = "Czy na pewno chcesz przekazać zadanie?";
+      } else {
+        this.dialogHeader = `Koniec zadania`;
+        this.dialogMessage = "Czy na pewno chcesz zakończyć zadanie?";
+      }
+      this.dialogPass = true;
+    },
+    getNextRole() {
+      const roles = this.$store.getters["role/find"]().data;
+      console.log(roles);
+      const nextRoleId =
+        roles.findIndex(
+          role =>
+            role.id === this.getTaskFromStore(this.$route.params.id).roleId
+        ) + 1;
+      return roles[nextRoleId];
+    },
+    onPassOnTask() {
+      // Set userId to null,
+      // Set roleId to the next role
+      const nextRole = this.getNextRole();
+      this.$store.dispatch("task/patch", [
+        this.$route.params.id,
+        { userId: null, roleId: nextRole ? nextRole.id : null }
+      ]);
+      this.dialogPass = false;
+    },
+    onRejectTask() {}
   }
 };
 </script>
