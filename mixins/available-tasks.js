82bc83bf4e92f770ab2ed796cbf81e29a9045ad4
s3ns3i <eragonsm@gmail.com>
2020-04-13 02:48:50 +0200

    Protection for /translations route

--- mixins/available-tasks.js
+++ mixins/available-tasks.js
@@ -8,17 +8,20 @@ export default {
   methods: {
     getAvailableTasks() {
       const { user } = this.$store.state.auth;
-      const roleIds = user.roles.map(role => role.id);
-      const projectIds = user.user_project_roles.map(userProjectRole => {
-        return userProjectRole.project_role.projectId;
-      });
-      return this.findTasksInStore({
-        query: {
-          roleId: { $in: roleIds },
-          projectId: { $in: projectIds },
-          $or: [{ userId: user.id }, { userId: null }]
-        }
-      }).data;
+      if (user) {
+        const roleIds = user.roles.map(role => role.id);
+        const projectIds = user.user_project_roles.map(userProjectRole => {
+          return userProjectRole.project_role.projectId;
+        });
+        return this.findTasksInStore({
+          query: {
+            roleId: { $in: roleIds },
+            projectId: { $in: projectIds },
+            $or: [{ userId: user.id }, { userId: null }]
+          }
+        }).data;
+      }
+      return []
     }
   }
 }
