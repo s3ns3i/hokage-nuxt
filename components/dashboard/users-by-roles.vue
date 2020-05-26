6397d42c1e099eab5d6a31d56b4040023fbccaf3
s3ns3i <eragonsm@gmail.com>
2020-05-26 16:44:51 +0200

    Improved styling

--- components/dashboard/users-by-roles.vue
+++ components/dashboard/users-by-roles.vue
@@ -1,37 +1,42 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col
-        v-for="(role, i) in items"
-        :key="role.id"
-        :class="['pr-0', { 'pl-0': i === 0 }]"
-      >
-        <v-card>
-          <v-card-title class="subtitle-1 primary lighten-1">{{
-            role.name
-          }}</v-card-title>
-          <v-divider />
-          <v-list v-if="role.users.length" dense>
-            <v-list-item
-              v-for="(user, j) in role.users"
-              :key="user.id"
-              :class="[
-                { 'primary lighten-4': j % 2 === 0 },
-                { 'primary darken-1': j % 2 === 0 && $vuetify.theme.dark }
-              ]"
-            >
-              <v-list-item-content>{{ user.nickname }}</v-list-item-content>
-            </v-list-item>
-          </v-list>
-          <v-list v-else>
-            <v-list-item class="font-weight-bold">
-              Brak danych
-            </v-list-item>
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card class="pb-4">
+    <v-card-title>
+      Użytkownicy przypisani do ról
+    </v-card-title>
+    <v-container>
+      <v-row class="px-4">
+        <v-col
+          v-for="(role, i) in items"
+          :key="role.id"
+          :class="['pr-0', { 'pl-0': i === 0 }]"
+        >
+          <v-card>
+            <v-card-title class="subtitle-1 primary lighten-1">{{
+              role.name
+            }}</v-card-title>
+            <v-divider />
+            <v-list v-if="role.users.length" dense>
+              <v-list-item
+                v-for="(user, j) in role.users"
+                :key="user.id"
+                :class="[
+                  { 'primary lighten-4': j % 2 === 0 },
+                  { 'primary darken-1': j % 2 === 0 && $vuetify.theme.dark }
+                ]"
+              >
+                <v-list-item-content>{{ user.nickname }}</v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-list v-else>
+              <v-list-item class="font-weight-bold">
+                Brak danych
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
